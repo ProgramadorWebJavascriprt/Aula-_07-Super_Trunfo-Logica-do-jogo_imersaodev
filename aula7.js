085ff7ba1139db96c78787d9f712ea713e224e01
André Luis Kunert <91467055+ProgramadorWebJavascriprt@users.noreply.github.com>
2021-09-30 09:59:51 -0300

    aula07_alura_irmersaodev
    Finalizei aula...

--- aula7.js
+++ aula7.js
@@ -41,5 +41,43 @@ var carta1 = {
     console.log(cartaJogador);
     document.getElementById("btnSortear").disabled = true;
     document.getElementById("btnJogar").disabled = false;
+  }// Quinta-feira,30/09/2021{amanhã} das 08:00:00 até as 09:55:00 h+|-
+function exibirOpcoes() {
+  var opcoes = document.getElementById("opcoes"); /*coloquei  {documment}*/
+  var opcoesTexto = "";
+
+  for (var atributo in cartaJogador.atributos) {
+    // console.log(atributo);
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      " '>" +
+      atributo;
   }
+  opcoes.innerHTML = opcoesTexto;
+}
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  // console.log(atributoSelecionado);
+  var elementoResultado = document.getElementById("resultado");
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+  if (valorCartaJogador > valorCartaMaquina) {
+    elementoResultado.innerHTML = "Você venceu";
+  } else if (valorCartaMaquina > valorCartaJogador) {
+    elementoResultado.innerHTML = "Você perdeu , a carta da Máquina é maior..";
+  } else {
+    elementoResultado.innerHTML = "Empatou";
+  }
+  console.log(valorCartaMaquina);
+  // console.log(cartaJogador.atributos[atributoSelecionado]);
+}
   
